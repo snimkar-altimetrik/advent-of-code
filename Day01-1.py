e1fd7908e5b2d7d4de83f8715b44a58d850529c0
snimkar-altimetrik <snimkar@altimetrik.com>
2022-12-01 16:47:35 +0530

    feat: Advent Day 01

---
+++ Day01-1.py
@@ -0,0 +1,35 @@
+"""
+Day 01: Elven Calories
+"""
+
+def get_sum_of_calories():
+    with open('input_1.txt', 'r') as input_file:
+        elves_calories = []
+        calories = []
+        for line in input_file.readlines():
+            line = line.strip()
+            if line !='':
+                calories.append(int(line))
+            else:
+                elves_calories.append(sum(calories))
+                calories = []
+    return elves_calories
+
+
+def get_sum_of_top_three_calories(all_calories):
+    top_three = sorted(all_calories)[-3:]
+    result = sum(top_three)
+    return result
+
+
+if __name__ == '__main__':
+
+    all_calorie = get_sum_of_calories()
+    max_calorie = max(all_calorie)
+    print(max_calorie)
+    top_three_sum = get_sum_of_top_three_calories(all_calorie)
+    print(top_three_sum)
+
+
+
+
